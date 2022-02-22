ffffaa3e838c17551334cd186ba84e1e4192cb6c
bun913 <imanau03@gmail.com>
2022-02-23 07:01:23 +0900

    feat: クライアント側の簡易的な実装を追加

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+import axios from 'axios';
 
 class Auth {
   constructor(userPoolId, clientId) {
@@ -9,7 +10,7 @@ class Auth {
     this.userPool = userPool;
   }
 
-  login(userName, password) {
+  async login(userName, password) {
     const authData = {
       Username: userName,
       Password: password,
@@ -21,21 +22,63 @@ class Auth {
       Pool: this.userPool,
     };
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.authenticateUser(authenticationDetails, {
-      onSuccess: (result) => {
-        console.log(result);
-      },
-      onFailure: (err) => {
-        console.log(err);
-      },
-      newPasswordRequired: function (_userAtt, _reqAtt) {
-        // 初回コンソールで作ったユーザーのみパスワード変更が必要
-        const newPassword = process.env.newPassword
-        cognitoUser.completeNewPasswordChallenge(newPassword, {'gender': 'Male', 'name': 'test'}, this)
-      }
+    return new Promise((resolve, reject) => {
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: async (result) => {
+          resolve(result);
+        },
+        onFailure: async (err) => {
+          reject(err);
+        },
+        newPasswordRequired: async function (_userAtt, _reqAtt) {
+          const newPassword = process.env.newPassword;
+          const result = cognitoUser.completeNewPasswordChallenge(
+            newPassword,
+            { gender: 'Male', name: 'test' },
+            this
+          );
+          resolve(result);
+        },
+      });
     });
   }
 }
 
-const auth = new Auth(process.env.userPoolID, process.env.CLIENTID);
-const result = auth.login(process.env.USER_NAME, process.env.PASS);
+class BaseRequest {
+  constructor(idToken) {
+    const axiosBase = axios.create({
+      baseURL: 'http://localhost:5000',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + idToken,
+      },
+      responseType: 'json',
+    });
+    this.requestClient = axiosBase;
+  }
+
+  async hello(option = null) {
+    try {
+      const res = await this.requestClient.get('/', option);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+async function auth() {
+  const auth = new Auth(process.env.userPoolID, process.env.CLIENTID);
+  const authResult = await auth.login(process.env.USER_NAME, process.env.PASS);
+  return authResult;
+}
+
+async function requestServer() {
+  const authToken = await auth();
+  const idToken = authToken.idToken.jwtToken;
+  const request = new BaseRequest(idToken);
+  return request.hello();
+}
+
+const helloResult = await requestServer();
+console.log(helloResult);
